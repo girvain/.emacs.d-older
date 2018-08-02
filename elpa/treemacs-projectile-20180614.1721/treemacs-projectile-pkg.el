caa42b5df08d9fa396edfaabd98b4c93c4f044b3
girvain <gavinross88@hotmail.co.uk>
2018-08-02 16:51:11 +0000

    ass

---
+++ elpa/treemacs-projectile-20180614.1721/treemacs-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-projectile" "20180614.1721" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0")) :commit "7b831e9d86cb8c964ebae10502823478c0c51d26" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
